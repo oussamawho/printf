6ec6ebd3116cb843fefd1d305c1d44f5f8751ec1
oussamawho <osmwho@gmail.com>
2023-09-22 17:20:19 -0700

    x

--- _precision.c
+++ _precision.c
@@ -2,11 +2,10 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
- *
- * Return: Precision.
+ * @format: Formatted string to print the arguments
+ * @i: List of arguments
+ * @list: list of arguments
+ * Return: Precision
  */
 int get_precision(const char *format, int *i, va_list list)
 {
