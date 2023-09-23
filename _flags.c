7237526351cb3c60f1feb3fc7ee791cec0822ab1
oussamawho <osmwho@gmail.com>
2023-09-22 17:47:05 -0700

    x

--- _flags.c
+++ _flags.c
@@ -8,25 +8,25 @@
  */
 int get_flags(const char *format, int *i)
 {
-	int j, curr_i;
+	int x, i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (i = *i + 1; format[i] != '\0'; i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (x = 0; FLAGS_CH[x] != '\0'; x++)
+			if (format[i] == FLAGS_CH[x])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[x];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[x] == 0)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*i = i - 1;
 
 	return (flags);
 }
