c732125bea547f7c76b7ef02b64ce85a3631de2d
oussamawho <osmwho@gmail.com>
2023-09-22 17:32:35 -0700

    x

--- utils.c
+++ utils.c
@@ -2,7 +2,8 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char
+ * @c: Char to be evaluated.
+ *
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
@@ -14,22 +15,24 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code
- * @buffer: Array
- * @i: Index at which to start appending
- * @ascii_code: ASSCI CODE
- * Return: 3
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
+ * @i: Index at which to start appending.
+ * @ascii_code: ASSCI CODE.
+ * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	char map[] = "0123456789ABCDEF";
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
-	ascii_code *= -1;
+		ascii_code *= -1;
+
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
 
-	buffer[i++] = map[ascii_code / 16];
-	buffer[i] = map[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
@@ -81,3 +84,4 @@ long int convert_size_unsgnd(unsigned long int num, int size)
 
 	return ((unsigned int)num);
 }
+
