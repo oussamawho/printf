e22d23d457953e1d0295116b9b3347597294ea86
oussamawho <osmwho@gmail.com>
2023-09-22 17:59:41 -0700

    x

--- _flags.c
+++ _flags.c
@@ -1,10 +1,10 @@
-include "main.h"
+#include "main.h"
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string to print the arguments
- * @i: take a parameter
- * Return: Flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
  */
 int get_flags(const char *format, int *i)
 {
@@ -32,3 +32,4 @@ int get_flags(const char *format, int *i)
 
 	return (flags);
 }
+
